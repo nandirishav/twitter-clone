8888145ad88a8564098f253653c114612951c5c7
RISHAV\USER <rishavnandi06@gmail.com>
2023-09-03 19:04:29 +0530

    fix

--- app/notifications/page.tsx
+++ app/notifications/page.tsx
@@ -1,11 +1,10 @@
 import Header from "@/components/Header";
 import NotificationsFeed from "@/components/NotificationsFeed";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
-import { NextPageContext } from "next";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
-const Notifications = async (context: NextPageContext) => {
+const Notifications = async () => {
   const session = await getServerSession(authOptions);
   if (!session) redirect("/");
   return (
